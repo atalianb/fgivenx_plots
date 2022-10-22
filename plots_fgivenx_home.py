e57193c00317d2e714afc79084b90d31f8219d1c
Atalia Navarro-Boullosa <atalia.navarro.boullosa@gmail.com>
2022-10-22 09:36:08 -0500

    mod to plot.py for next galaxy

--- plots_fgivenx_home.py
+++ plots_fgivenx_home.py
@@ -153,17 +153,17 @@ def Vc_interpol(r,X,Vc):
     Vc_new = f(r)
     return Vc_new
 ####3
-data_path = "/home/atalia/New_simpleMC_version/simplemc/data/Blok_McGaugh_&_Rubin_(2001)/"
-data = np.loadtxt(data_path+'U11616.dat')
-Galaxy_name = 'UGC11616'
+data_path = "/Users/atalianb/Documents/data_LBSG/data_used_by_Tula/"
+data = np.loadtxt(data_path+'U11748.dat')
+Galaxy_name = 'UGC11748'
 vecRp_data = np.array([row[1] for row in data])# galactocentric distance [kpc]
 vecvRp_data = np.array([row[5] for row in data])# rotation velocity [km/s]
 vecerrvRp_data = np.array([row[6] for row in data])# error in rotation velocity [km/s]
-params = np.array([10**(-2.3272415E+01),10**(-3.2506169E+00),
-                10**(-1.0047396E-02),10**(-1.7274918E+00),
-                10**(-9.5043103E-01)])
-path_nested = '/home/atalia/fgivenx_plots/chains/U11616'
-chains = np.loadtxt(path_nested+'/Rotation_phy_RC_nested_multi_1.txt')
+params = np.array([10**(-2.3708986E+01),10**(-3.0034393E+00),
+                10**(1.8800781E-01),10**(-5.4776401E+00),
+                10**(-1.3336016E+00)])
+path_nested = '/Users/atalianb/Documents/Doctorado/fgivenx_plots/chains/U11748'
+chains = np.loadtxt(path_nested+'/Rotation_phy_RC_nested_dynesty_multi_1.txt')
 phi0 = params[2]
 phi1 = params[3]
 phi2 = params[4]
@@ -196,11 +196,11 @@ Vc_l2 = Vc_interpol(vecRp_data,X012,Vc2_l2)
 #####
 ##
 #######
-m_a_new = 10.**(chains.T[2][835:4175])
-eps_new = 10.**(chains.T[3][835:4175])
-phi0_new = 10.**(chains.T[4][835:4175])
-phi1_new = 10.**(chains.T[5][835:4175])
-phi2_new = 10.**(chains.T[6][835:4175])
+m_a_new = 10.**(chains.T[2][2086:10428])
+eps_new = 10.**(chains.T[3][2086:10428])
+phi0_new = 10.**(chains.T[4][2086:10428])
+phi1_new = 10.**(chains.T[5][2086:10428])
+phi2_new = 10.**(chains.T[6][2086:10428])
 samples = np.array([(Anfw,rs,phi0,phi1,phi2) for Anfw,rs,phi0,phi1,phi2 in zip(m_a_new,eps_new,phi0_new,phi1_new,phi2_new)]).copy()
 ####
 nx = 100
@@ -209,7 +209,7 @@ def PPS(r,theta):
     Anfw,rs,phi0,phi1,phi2=theta
     Vc = Vc_m_a_eps_l012(r,theta)
     return Vc
-cbar = plot_contours(PPS,x,samples,contour_line_levels=[1,2],colors=plt.cm.Greys_r,alpha=0.8,parallel=22)#,ny=100)
+cbar = plot_contours(PPS,x,samples,contour_line_levels=[1,2],colors=plt.cm.Greys_r,alpha=0.8,parallel=7)#,ny=100)
 cbar = plt.colorbar(cbar,ticks=[0,1,2])
 cbar.set_ticklabels(['',r'$1\sigma$',r'$2\sigma$'])
 plt.ylabel(r'$v_{c}(r)$[km/s]')
@@ -222,5 +222,5 @@ plt.plot(X0_units,np.sqrt(Vc2_l2),label=r'$\psi_{320}$',linewidth=2.5)
 plt.legend(loc='lower right')
 plt.xlim(0,vecRp_data[-1])
 plt.title(Galaxy_name)
-plt.ylim(top=165)
+plt.ylim(top=350)
 plt.savefig('fgivenx_VcMultiL012_'+Galaxy_name+'_parallel_20porcent_burnin.pdf')
